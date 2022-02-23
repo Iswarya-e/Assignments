08d642efe6fb995a2b08e883c3068f1b2d7a164e
Iswarya-e <42031606+Iswarya-e@users.noreply.github.com>
2022-02-23 18:32:25 +0530

    Add files via upload

---
+++ Assignment 7/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+
+{
+    public class EmployeeController : Controller
+    {
+       
+        // GET: Employee
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult DisplayAll()
+        {
+                List<Employee> empList = new List<Employee>{
+                new Employee() { id = 101, Name = "ABC", Age = 44,City="Mumbai" } ,
+                new Employee() { id = 102, Name = "BCD", Age = 55,City="Mumbai" } ,
+                new Employee() { id = 103, Name = "CDE", Age = 33,City="Mumbai" }
+
+            };
+            TempData["EmployeeList"] = empList;
+            //TempData.Keep();
+
+            return View(empList);
+        }
+        public ActionResult Display2()
+        {            
+            return View(TempData["EmployeeList"]);
+        }
+    }
+}
---
+++ Assignment 7/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+namespace mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+            List<Product> Ilist = new List<Product>{ 
+                new Product(){ Id=101,Name="ABC"},
+                new Product(){ Id=102,Name="BCD"}
+            };
+        return View(Ilist);
+        }
+        public ActionResult Student()
+            
+        {
+            ViewBag.Message =  TempData["Message"].ToString();
+           
+            return View();
+        }
+       
+
+    }
+}
