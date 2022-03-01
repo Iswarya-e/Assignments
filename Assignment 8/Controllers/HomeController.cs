9fba186ecfc56e09689173ed3daf3d6923d36650
Iswarya-e <42031606+Iswarya-e@users.noreply.github.com>
2022-03-01 17:09:56 +0530

    Add files via upload

---
+++ Assignment 8/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment8.Models;
+
+namespace Assignment8.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Employee> list = new List<Employee>();
+
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee e)
+        {
+            if (ModelState.IsValid)
+            { 
+                if (e.Age<18)
+                {
+                    return RedirectToAction("errorAge");
+                }
+                else if (e.City == "Mumbai")
+                {
+                    return RedirectToAction("Index2",e);
+                }
+                else
+                {
+                    return RedirectToAction("errorCity");
+                }
+            }
+            return View(e);
+        }
+
+        public ActionResult Index2(Employee e)
+        {
+            list.Add(new Employee { Id=e.Id,Name=e.Name,Age=e.Age,City=e.City});
+            //ViewBag.Id=e.Id;
+            //ViewBag.Name = e.Name;
+            //ViewBag.Age = e.Age;
+            //ViewBag.City = e.City;
+            return View(list);
+        }
+        public ActionResult errorAge()
+        {
+            return View();
+        }
+        public ActionResult errorCity()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Assignment 8/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment8.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Id should not be null")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name should not be null")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Age should not be null")]
+        [Range(18, 100, ErrorMessage = "Age should be between 18 and 100")]
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
